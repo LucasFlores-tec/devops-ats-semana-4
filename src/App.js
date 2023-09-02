5cb1b7826716bead201cec1b03acb073a017af1e
Lucas Flores <lucas.flores@valtech.com>
2023-09-02 20:30:16 -0300

    Primeiro commit da Atividade Somativa - entrega semana 04

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello there! Este é o projeto da Atividade Somativa (Semana 4) da disciplina de DevOps (11100011130_20232_01)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
